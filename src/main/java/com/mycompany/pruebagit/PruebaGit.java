b10839e99c5d4e65714adfe2305ebcc077f0411d
Rodrigo022 <117395622+Rodrigo022@users.noreply.github.com>
2023-03-01 21:22:22 -0300

    Update PruebaGit.java

--- src/main/java/com/mycompany/pruebagit/PruebaGit.java
+++ src/main/java/com/mycompany/pruebagit/PruebaGit.java
@@ -12,5 +12,9 @@ public class PruebaGit {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        for(int i = 0; i < 2; i++){
+          System.out.println("Hola Mundo");
+        }
     }
 }
