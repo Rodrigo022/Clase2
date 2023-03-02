4387c69ad2da646567b1d305a58c739bbf7f0116
Rodrigo Gonzalez <gonzalezrodrigo0020@gmail.com>
2023-03-01 21:25:26 -0300

    Comente un for

--- src/main/java/com/mycompany/pruebagit/PruebaGit.java
+++ src/main/java/com/mycompany/pruebagit/PruebaGit.java
@@ -13,8 +13,8 @@ public class PruebaGit {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-        for(int i = 0; i < 2; i++){
-          System.out.println("Hola Mundo!");
-        }
+//        for(int i = 0; i < 2; i++){
+//          System.out.println("Hola Mundo!");
+//        }
     }
 }
